314af42b7ea3d3deaa03de1400d389f9535e6584
PSYMURAI <kakadedhananjay59@gmail.com>
2025-03-08 16:19:19 +0530

    feat added redux slice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import userDashboard from './pages/user/dashboard/userDashboard';
+import ProtectedRoute from './middleware/protectedRoute';
+import Home from './pages/default/home';
+import LoginPage from './pages/user/login/login';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<ProtectedRoute><userDashboard /></ProtectedRoute>} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
